26397cf705447edeae6d1a93b2cf3ad4d4b6bcc6
GraysonDomoleczny <gdomoleczny3@gmail.com>
2026-01-08 21:13:51 -0600

    1/8/2025

--- Python/RobloxAutoServer.py
+++ Python/RobloxAutoServer.py
@@ -1,60 +1,51 @@
-import pyautogui
-pyautogui.PAUSE = 2
-pyautogui.FAILSAFE = True
+import pyautogui as pg
+from pyscreeze import ImageNotFoundException
+
+pg.PAUSE = 2
+pg.FAILSAFE = True
 import time
 
 def main():
-    #test2
-
     def checkCursorPosition():
         time.sleep(2)
-        print(pyautogui.position())
+        print(pg.position())
         quit()
 
-    def findImages():
-        # should be roughly 550, 330
-        print(pyautogui.locateCenterOnScreen('changename.png', confidence=0.95))
 
     def privateServerCheck():
-        #navigate to private server settings
-        pyautogui.click(280, 394)
-        pyautogui.scroll(-500)
-        pyautogui.click(883, 732)
-        pyautogui.click(883, 775)
+        # Navigates to private server settings
+        pg.click(pg.locateOnScreen('servers.png', confidence=0.95), clicks=2)
+        pg.scroll(-700)
+        pg.click(pg.locateOnScreen('threedots.png', confidence=0.95))
+        pg.click(pg.locateOnScreen('configbutton.png', confidence=0.95))
 
-        #change name
-        #position of edit button on name changes with length of name
-        pyautogui.click(pyautogui.locateCenterOnScreen('changename.png', confidence=0.95))
-        pyautogui.click(657, 582)
-        pyautogui.press('backspace', presses=50)
-        pyautogui.write('This is a changed name!')
-        pyautogui.click(418, 641)
+        # Disable server joining if it is current enabled
+        try:
+            pg.click(pg.locateOnScreen('toggle.png', confidence=0.95))
+        except pg.ImageNotFoundException:
+            pass
 
-        #return to inventory
-        pyautogui.click(21, 63)
-        pyautogui.click(21, 63)
+        # Changes server name
+        pg.click(pg.locateOnScreen('changenameicon.png', confidence=0.95))
+        pg.click(pg.locateOnScreen('namechangebox.png', confidence=0.95))
+        pg.press('backspace', presses=50)
+        pg.write('This is a changed name!')
+        pg.click(pg.locateOnScreen('changename.png', confidence=0.95))
+
+        # Returns to inventory
+        pg.click(pg.locateOnScreen('backarrow.png', confidence=0.95), clicks=2)
 
 
     def gameIndexLoop():
-        # monitor size 1920 x 1080
-        # 138x 245y between roblox games on half screen monitor
-        # starting 280x 394y
-        vertical_index = 0
-        y_offset = 394
-        while vertical_index < 3:
-            horizontal_index = 0
-            x_offset = 280
-            while horizontal_index < 5:
-                print(pyautogui.position())
-                pyautogui.moveTo(x_offset, y_offset)
-                x_offset += 138
-                horizontal_index += 1
-            y_offset += 245
-            vertical_index += 1
+        for location in pg.locateAllOnScreen('atsymbol.png', confidence=0.90):
+            pg.click(location[0], location[1] - 80)
+            privateServerCheck()
+
 
     #checkCursorPosition()
     #findImages()
     privateServerCheck()
+    #gameIndexLoop()
 
 main()
 
